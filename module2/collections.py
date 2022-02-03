587bd40fcd4ecbf16314d43a92f742abd99515c5
Denis Moiseev <denis_moiseev@epam.com>
2022-02-03 14:37:43 +0300

    PR comments have been fixed.

--- module2/collections.py
+++ module2/collections.py
@@ -6,7 +6,7 @@ import string
 random_dict = dict()
 random_list = []
 for i in range(random.randint(2, 10)):
-    for j in range(random.randint(0, 10)):
+    for j in range(random.randint(0, len(string.ascii_lowercase))):
         random_dict[random.choice(string.ascii_lowercase)] = random.randint(0, 1000)
     random_list.append(random_dict.copy())
     random_dict.clear()
@@ -15,23 +15,19 @@ for i in range(random.randint(2, 10)):
 # Collect values by key from all dictionaries.
 aggr_dict = dict()
 for dic in random_list:
-    for key in dic.keys():
-        if key not in aggr_dict.keys():
-            aggr_dict[key] = [dic[key]]
+    for k, v in dic.items():
+        if k not in aggr_dict.keys():
+            aggr_dict[k] = [v]
         else:
-            aggr_dict[key].append(dic[key])
-
-# Find max value for each key.
-max_val_dict = dict.fromkeys(aggr_dict)
-for key in aggr_dict:
-    max_val_dict[key] = max(aggr_dict[key])
+            aggr_dict[k].append(v)
 
 # Find index of dictionary where key:max(value) exist and rename key if key is not unique.
-final_dict = max_val_dict.copy()
-for key in max_val_dict:
+final_dict = dict.fromkeys(aggr_dict)
+for key in aggr_dict.keys():
+    final_dict[key] = max(aggr_dict[key])
     for dct in random_list:
-        if key in dct.keys():
-            if max_val_dict[key] == dct[key] and len(aggr_dict[key]) != 1:
+        if key in dct.keys() and key in final_dict.keys():
+            if max(aggr_dict[key]) == dct[key] and len(aggr_dict[key]) != 1:
                 new_key = key + '_' + str(random_list.index(dct) + 1)
                 final_dict[new_key] = final_dict.pop(key)
 
